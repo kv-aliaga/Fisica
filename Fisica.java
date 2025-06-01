5e2ac5b154ac55cc9e6f2dd1eb077ccc40004f45
davi <davi.ramos@germinare.org.br>
2025-06-01 12:39:25 -0300

    Alterando diagramação em Fisica.java

--- Fisica.java
+++ Fisica.java
@@ -5,13 +5,12 @@ public class Fisica{
     public static void main(String[] args) { // iniciando main
         Scanner input = new Scanner(System.in);
 
-        // variáveis
-        double segundos, alturaInicial; // variáveis de input
-        double alturaFinal, velocidadeFinal, deslocamentoVertical; // variáveis de cálculo
-        double aceleracao = 0;
+        // variáveis de conta
 
-        boolean certo = false;
-        
+        double segundos, alturaInicial, aceleracao; // variáveis de input
+        double alturaFinal, velocidadeFinal, deslocamentoVertical; // variáveis de cálculo
+        boolean certo = false; // variável de confirmação
+
         // cores
         String AZUL = "\u001B[34m";
         String RESET = "\u001B[0m";
@@ -21,6 +20,13 @@ public class Fisica{
 
         // backgrounds
         String BRANCO_BACKGROUND = "\u001B[47m";
+
+        // variáveis de diagramação
+
+        String resultado = "=== RESULTADO ===";
+        int largura = 160;
+        int espacos = (largura - resultado.length()) / 2;
+        String linha = "=".repeat(largura);
 
         // iniciando loop
         do{
@@ -41,36 +47,38 @@ public class Fisica{
                         [1] 9,80665 m/s²
                         [2] 9,8 m/s²
                         [3] 9,81 m/s²
-                        [4] 10 m/s²
-                        """);
-                        
-                        aceleracao = input.nextDouble();
+                        [4] 10 m/s²""");
 
-                        if (aceleracao == 1){
-                            aceleracao = 9.80665;
-                        } else if (aceleracao == 2){
-                            aceleracao = 9.8;
-                        } else if (aceleracao == 3){
-                            aceleracao = 9.81;
-                        } else if (aceleracao == 4){
-                            aceleracao = 10;
-                        } else{aceleracao = 0;
-                            System.out.println(VERMELHO + "Opção inválida.\n" + RESET);
-                        }
+                    aceleracao = input.nextDouble();
+
+                    if (aceleracao == 1){
+                        aceleracao = 9.80665;
+                    } else if (aceleracao == 2){
+                        aceleracao = 9.8;
+                    } else if (aceleracao == 3){
+                        aceleracao = 9.81;
+                    } else if (aceleracao == 4){
+                        aceleracao = 10;
+                    } else{aceleracao = 0;
+                        System.out.println(VERMELHO + "Opção inválida.\n" + RESET);
+                    }
                 } while (aceleracao == 0);
 
-                    // cálculos
-                    alturaFinal = calcularAltura(alturaInicial, segundos, aceleracao);
-                    velocidadeFinal = calcularVelocidade(segundos, aceleracao);
-                    deslocamentoVertical = alturaInicial - alturaFinal;
+                // cálculos
+                alturaFinal = calcularAltura(alturaInicial, segundos, aceleracao);
+                velocidadeFinal = calcularVelocidade(segundos, aceleracao);
+                deslocamentoVertical = alturaInicial - alturaFinal;
 
-                    // Resultados
-                    System.out.println(PRETO + BRANCO_BACKGROUND + "\n=== RESULTADOS ===" + RESET);
-                    System.out.printf("\nVelocidade final: " + AZUL + "%.2f m/s" + RESET, velocidadeFinal);
-                    System.out.printf("\nAltura final: " + AZUL + "%.2f m" + RESET, alturaFinal);
-                    System.out.printf("\nDeslocamento vertical: " + AZUL + "%.2f m\n" + RESET, deslocamentoVertical);
+                // Resultados
+                System.out.println("\n" + BRANCO_BACKGROUND + PRETO + linha + RESET);
+                System.out.println(BRANCO_BACKGROUND + PRETO + " ".repeat(espacos) + resultado + " ".repeat(largura - resultado.length() - espacos) + RESET);
+                System.out.println(BRANCO_BACKGROUND + PRETO + linha + RESET);
 
-                    certo = true; // encerra o loop
+                System.out.printf("\nVelocidade final: " + AZUL + "%.2f m/s" + RESET, velocidadeFinal);
+                System.out.printf("\nAltura final: " + AZUL + "%.2f m" + RESET, alturaFinal);
+                System.out.printf("\nDeslocamento vertical: " + AZUL + "%.2f m\n" + RESET, deslocamentoVertical);
+
+                certo = true; // encerra o loop
 
             } catch (InputMismatchException ime) {
                 System.out.println(VERMELHO + "\nDigite apenas números.\n" + RESET);
@@ -79,7 +87,7 @@ public class Fisica{
 
         } while (! certo);
     }   // fechando main 
-    
+
     public static double calcularAltura(double alturaInicial, double tempo, double aceleracao) { // iniciando calcularAltura
         return alturaInicial - 0.5 * aceleracao * Math.pow(tempo, 2);
     } // fechando calcularAltura
