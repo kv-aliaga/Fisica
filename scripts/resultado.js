6ef8521556c6054f4ec925dadebe40570b23f969
Lucas Kluska Donini <lkd.lucas@gmail.com>
2025-06-09 07:03:15 -0300

    formatei a saída

--- scripts/resultado.js
+++ scripts/resultado.js
@@ -3,4 +3,4 @@ const result = localStorage.getItem('result');
 const unidade = localStorage.getItem('unidade');
 const grandeza = localStorage.getItem('grand');
 
-result_field.text(`${grandeza} = ${result} ${unidade}`);
+result_field.text(`${grandeza} = ${Number(result).toLocaleString('pt-BR', {minimumFractionDigits:0, maximumFractionDigits:2})} ${unidade}`);
